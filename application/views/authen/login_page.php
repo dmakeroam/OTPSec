2b1d6dad0d51e53e7e18a0bcd0244d821e278bcb
Dreammaker_Oam <cpfapp@gmail.com>
2015-11-01 00:49:18 +0900

    Added required attributes to the username field

--- application/views/authen/login_page.php
+++ application/views/authen/login_page.php
@@ -21,13 +21,13 @@
   <div class="row">
     <div class="Absolute-Center is-Responsive">
       <div class="col-sm-12 col-md-10 col-md-offset-1">
-        <form action="" id="loginForm">
+        <form action="" id="loginForm" method="post">
           <div class="form-group input-group">
             <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-            <input class="form-control" type="text" name='username' placeholder="username"/>          
+            <input class="form-control" type="text" name='username' placeholder="Username" required="true"/>          
           </div>
           <div class="form-group">
-            <button type="button" class="btn btn-def btn-block">Login</button>
+            <button type="submit" class="btn btn-def btn-block">Login</button>
           </div>
         </form>        
       </div>  
