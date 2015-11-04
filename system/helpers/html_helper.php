c6a5222a7d548ea322097b81b597c466d6004753
maxnus <cpfapp@gmail.com>
2015-11-04 21:04:29 +0900

    Modified html_helper in the system files of CI to use script_tag function

--- system/helpers/html_helper.php
+++ system/helpers/html_helper.php
@@ -333,6 +333,60 @@ if ( ! function_exists('link_tag'))
 	}
 }
 
+if ( ! function_exists('script_tag')) {
+    
+    /**
+	 * Script
+	 *
+	 * Generates link to a javascript file
+	 *
+	 * @param	mixed	javascript src or an array
+	 * @param	string	language
+	 * @param	string	type
+	 * @param	bool	should index_page be added to the javascript path
+	 * @return	string
+	 */
+    
+    
+    function script_tag($src = '', $language = 'javascript', $type = 'text/javascript', $index_page = FALSE)
+    {
+        $CI =& get_instance();
+        $script = '<scr'.'ipt';
+        if (is_array($src)) {
+            foreach ($src as $k=>$v) {
+                if ($k == 'src' AND strpos($v, '://') === FALSE) {
+                    if ($index_page === TRUE) {
+                        $script .= ' src="'.$CI->config->site_url($v).'"';
+                    }
+                    else {
+                        $script .= ' src="'.$CI->config->slash_item('base_url').$v.'"';
+                    }
+                }
+                else {
+                    $script .= "$k=\"$v\"";
+                }
+            }
+
+            $script .= "></scr"."ipt>\n";
+        }
+        else {
+            if ( strpos($src, '://') !== FALSE) {
+                $script .= ' src="'.$src.'" ';
+            }
+            elseif ($index_page === TRUE) {
+                $script .= ' src="'.$CI->config->site_url($src).'" ';
+            }
+            else {
+                $script .= ' src="'.$CI->config->slash_item('base_url').$src.'" ';
+            }
+
+            $script .= 'language="'.$language.'" type="'.$type.'"';
+            $script .= ' /></scr'.'ipt>'."\n";
+        }
+        return $script;
+    }
+}
+
 // ------------------------------------------------------------------------
 
 if ( ! function_exists('meta'))
