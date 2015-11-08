b556ced2f02f9617e439d9c757a00243e56a4554
maxnus <cpfapp@gmail.com>
2015-11-08 14:23:56 +0900

    Deleted error from login for more security

--- application/views/authen/login_page.php
+++ application/views/authen/login_page.php
@@ -5,9 +5,6 @@ if ( ! defined('BASEPATH')) exit('No direct script access allowed');
   <div class="row">
     <div class="Absolute-Center is-Responsive">
       <div class="col-sm-12 col-md-10 col-md-offset-1">
-        <?php if(isset($error)):?>
-        <p style='text-align:center; color:red;'><?=$error?></p>
-        <?php endif;?>
          <?=form_open('',array('id'=>'login_form','onsubmit'=>'return showProgressBar()'))?>
           <div class="form-group input-group">
             <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
