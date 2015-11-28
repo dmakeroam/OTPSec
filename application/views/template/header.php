a6288707a76a848c1384e4d3a4d90d049ba15023
maxnus <cpfapp@gmail.com>
2015-11-28 23:34:51 +0900

    Added the link to the login page from the registration page

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -15,20 +15,22 @@ if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 <?php endif;?>
 <?=link_tag('assets/css/main/style.css')?>
 <?=link_tag('https://cdnjs.cloudflare.com/ajax/libs/flat-ui/2.2.2/css/flat-ui.min.css')?>
-<script src='https://www.google.com/recaptcha/api.js'></script>
+<?=script_tag('https://www.google.com/recaptcha/api.js')?>
 </head>
 <body>
 <div class="navbar navbar-fixed-top navbar-default">
     <div class="navbar-inner">
         <div class="container">
         <nav class='navbar-brand'>
-            <a href='<?=base_url('')?>' class='a_link'><?php if(isset($page) && $page==='member') :?>OTPSec Member<?php else:?><?php if(isset($page) && $page==='regis'):?>OTPSec Registration<?php else:?>OTPSec Authentication<?php endif;?><?php endif;?></a>
+            <a href='<?php if(isset($page) && $page==='member') :?><?=base_url('')?><?php else:?><?php if(isset($page) && $page==='regis'):?><?=base_url('main/registration')?><?php else:?><?=base_url('authen/login')?><?php endif;?><?php endif;?>' class='a_link'><?php if(isset($page) && $page==='member') :?>OTPSec Member<?php else:?><?php if(isset($page) && $page==='regis'):?>OTPSec Registration<?php else:?>OTPSec Authentication<?php endif;?><?php endif;?></a>
         </nav>
+        <nav class='navbar-brand-right'>   
         <?php if(isset($page) && $page==='member') :?>
-        <nav class='navbar-brand-right'>
         Hi, <?=$username?>!
-        </nav>
+        <?php elseif (isset($page) && $page!=='login') :?>
+        <?=anchor('main','Login','class="a_link"')?> 
         <?php endif;?>
+         </nav>
         </div>
     </div>
 </div> 
